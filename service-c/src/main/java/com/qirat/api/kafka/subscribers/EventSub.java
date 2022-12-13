cff5eef1c7fa84dc0d45ec4ad251968cbd599aad
wijzijrd <the.mystical.wizord@gmail.com>
2022-12-13 22:23:08 +0200

    Initial implementation for service-c

---
+++ service-c/src/main/java/com/qirat/api/kafka/subscribers/EventSub.java
@@ -0,0 +1,23 @@
+package com.qirat.api.kafka.subscribers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class EventSub {
+    private final ObjectMapper mapper;
+
+    @KafkaListener(topics = "service-d-resp", groupId = "group-1")
+    public void listen(final String message) throws JsonProcessingException {
+        log.info("Received Message in group-1: {}" , message);
+
+    }
+
+}
